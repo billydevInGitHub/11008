9e4e3449aaf6981440ef599c63e2db9666ec3aea
billy <billydev@gmail.com>
2020-03-21 13:06:15 -0600

    this is third  commit

--- Exp002_IDETest/src/main/java/idetest/Test.java
+++ Exp002_IDETest/src/main/java/idetest/Test.java
@@ -24,8 +24,10 @@ public class Test implements TestInterface, TestInterface2, Interface3, interfac
 
     @Override
     public String toString() {
-        
-        
+
+        stringtestnew = "Test`{}asdasdf";
+
+        stringtestnew = "Test`{}";
         stringtestnew = "Test`{}";
         return stringtestnew;
     }
@@ -42,18 +44,32 @@ public class Test implements TestInterface, TestInterface2, Interface3, interfac
 
         System.out.println("this is test1");
 
-         System.out.println("edit on git hub");
-         System.out.println("edit on git hub new change from github2");
+        System.out.println("edit on git hub");
+        System.out.println("edit on git hub new change from github2");
         System.out.println("edit on git hub new change from github3");
         System.out.println(test2);
 
+        Test2 tt2 = new Test2();
+
         Test2.testRefactoringAAA = String.valueOf(5);
-        int j=9;
+        int j = 9;
         for (int i = 0; i < j; i++) {
 
         }
         List<String> list;
 
+        int jj = 9;
+        for (int i = 0; i < jj; i++) {
+
+        }
+
+        String s = null;
+        if (!s.equals("sfs")) {
+
+        }
+        s.compareTo("sdf");
+
+        System.out.println(s.toLowerCase());
         System.out.println(test2);
 
     }
